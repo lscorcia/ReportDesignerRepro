c7032092d9d9977ab04846dd071dbd65a569ba46
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2021-11-23 14:06:47 +0100

    Add project files.

---
+++ MiSE.Backend/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiSE.Backend
+{
+    public class Class2
+    {
+        public List<int> GetItems()
+        {
+            return new List<int>() { 1, 2, 3, 4, 5 };
+        }
+    }
+}
---
+++ MiSE.Infrastructure/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiSE.Infrastructure
+{
+    public class Class1
+    {
+        public List<int> GetItems()
+        {
+            return new List<int>() { 1, 2, 3, 4, 5 };
+        }
+    }
+}
